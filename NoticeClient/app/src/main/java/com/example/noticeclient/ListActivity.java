1d6127a1d660eb6d6e52b3f223aec4c3957e64d6
zino <zino1187@gmail.com>
2023-02-17 15:20:23 +0900

    dd

--- NoticeClient/app/src/main/java/com/example/noticeclient/ListActivity.java
+++ NoticeClient/app/src/main/java/com/example/noticeclient/ListActivity.java
@@ -2,9 +2,13 @@ package com.example.noticeclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,5 +46,21 @@ public class ListActivity extends AppCompatActivity {
 
         //javaSE 처럼  new JTable(model) 와 같이 뷰와 컨트롤러를 연결해야 한다.l
         listView.setAdapter(noticeAdapter);
+
+        //글쓰기 버튼 이벤트 연결
+        Button bt_regist = findViewById(R.id.bt_regist);
+        bt_regist.setOnClickListener((v)->{
+            //안드로이드의 Activity는 시스템이 관리하기 때문에 개발자가 new 할수 없다.
+            //시스템에 의해서 관리되는 Activity는 생명주기가 존재하고, 그 생명주기 에 맞게
+            //개발자는 적절한 코드를 작성만 하면 된다..
+            // 마치 웹에서의 서블릿을 서버가 관리하는 것처럼.........
+
+            //개발자는 안드로이드 시스템에 무엇을 원하는지 의도를 보여야 하는데,
+            Intent intent=new Intent(this, RegistActivity.class);
+            startActivity(intent);
+
+
+
+        });
     }
 }
--- NoticeClient/app/src/main/java/com/example/noticeclient/RegistActivity.java
+++ NoticeClient/app/src/main/java/com/example/noticeclient/RegistActivity.java
@@ -3,12 +3,131 @@ package com.example.noticeclient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class RegistActivity extends AppCompatActivity {
+    private String TAG=this.getClass().getName();
 
-    @Override
+    EditText t_title, t_writer, t_content;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_regist);
+
+        Button bt_regist = findViewById(R.id.bt_regist);
+        t_title = findViewById(R.id.t_title);
+        t_writer = findViewById(R.id.t_writer);
+        t_content = findViewById(R.id.t_content);
+
+        bt_regist.setOnClickListener((v)->{
+            regist();
+        });
     }
+
+    public void regist(){
+        Thread thread = new Thread(){
+            public void run() {
+                // Http Request
+                BufferedWriter buffw=null;
+                OutputStreamWriter os=null;
+                DataOutputStream dos=null;
+
+                BufferedReader buffr=null;
+                InputStreamReader is=null;
+
+                try {
+                    URL url = new URL("http://172.30.1.27:7777/rest/notice/regist");
+                    URLConnection uCon=url.openConnection();
+                    HttpURLConnection httpCon=(HttpURLConnection) uCon;
+                    httpCon.setRequestMethod("POST");
+                    httpCon.setDoOutput(true);
+                    httpCon.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+                    //보낼 데이터
+                    String title=t_title.getText().toString();
+                    String writer=t_writer.getText().toString();
+                    String content=t_content.getText().toString();
+
+                    String postData="title="+title+"&writer="+writer+"&content="+content;
+                    os = new OutputStreamWriter(httpCon.getOutputStream(), "UTF-8");
+                    buffw=new BufferedWriter(os);
+                    //dos = new DataOutputStream(httpCon.getOutputStream());
+                    //dos.writeBytes(postData);
+                    buffw.write(postData+"\n");
+                    buffw.flush();
+
+                    //데이터 요청 이후에 입력스트림 만들어야 한다..
+                    is = new InputStreamReader(httpCon.getInputStream(), "UTF-8");
+                    buffr = new BufferedReader(is);
+
+                    StringBuilder sb=new StringBuilder();
+                    while(true){
+                        String result=buffr.readLine();
+                        sb.append(result);
+                        if(result==null){
+                            break;
+                        }
+                    }
+                    Log.d(TAG, sb.toString());
+
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    if(is!=null){
+                        try {
+                            is.close();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    if(buffr!=null){
+                        try {
+                            buffr.close();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    if(os!=null){
+                        try {
+                            os.close();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    if(buffw!=null){
+                        try {
+                            buffw.close();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+
+                }
+            }
+        };
+        thread.start();
+    }
+
 }
+
+
+
+
+
+
